f478f3b13004b374fa4bd719af5c3e4d3410faa5
Bhavya Sree <2494368@cognizant.com>
2026-05-21 09:56:14 +0530

    Added the coach profiles

---
+++ backend/mfrp_cm/src/main/java/com/coachmetrics/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.coachmetrics.controller;
+
+import com.coachmetrics.dto.AuthDto;
+import com.coachmetrics.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthDto.AuthResponse> login(@RequestBody AuthDto.LoginRequest req) {
+        return ResponseEntity.ok(authService.login(req));
+    }
+}
